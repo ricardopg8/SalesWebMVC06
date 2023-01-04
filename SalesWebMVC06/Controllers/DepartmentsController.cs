afc201c5e8d63ef190cd5da81be5829a5912ff48
ricardopg8 <ricardopg8@gmail.com>
2023-01-04 07:19:27 -0300

    Inclusao da Controller Vazia (DepartmentsController)

---
+++ SalesWebMVC06/Controllers/DepartmentsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMVC06.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
